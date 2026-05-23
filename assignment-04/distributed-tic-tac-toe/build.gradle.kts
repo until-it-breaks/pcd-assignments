68531aa8118b6fa1edb14efbdc1982d728d83f91
Jiekai Sun <jiekai.sun@studio.unibo.it>
2026-05-24 00:25:58 +0200

    chore: set up project

---
+++ assignment-04/distributed-tic-tac-toe/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "it.unibo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ assignment-04/distributed-tic-tac-toe/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "distributed-tic-tac-toe"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
